32f78efc06e2c7f7a1a61fd154e62975d9280720
Ronak Siddhpura <107002793+RonakSiddhpura@users.noreply.github.com>
2025-09-15 17:17:49 +0530

    Update index.js

--- bingo-server/index.js
+++ bingo-server/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 // });
 const io = new Server(server, {
   cors: {
-    origin: ["https://bingo-game-frontend.onrender.com","https://bingo.ronaksiddhpura.tech"]
+    origin: ["https://bingo-game-frontend.onrender.com","https://bingo.ronaksiddhpura.tech"],
     methods: ["GET", "POST"]
   },
   transports: ["websocket","polling"]
@@ -179,6 +179,7 @@ server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 
 });
+
 
 
 
